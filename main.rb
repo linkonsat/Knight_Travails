6656967e9fd2c59deae92fa9087369098b486c3c
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-11-04 10:59:53 -0700

    redid instance variables to include night and set display to an instance variable

--- main.rb
+++ main.rb
@@ -3,34 +3,42 @@ require "./Chess_Objects/Knight"
 require "./Chess_Objects/Display"
 
 class Main
-    attr_accessor :main_board
+    attr_accessor :main_board, :main_display, :new_knight
     def initialize
         @main_board = GameBoard.new
     end
 
     def home_screen
         main_display = Display.new()
-        main_display.board_display
-        main_display.instructions
-        knight_directions(main_display)
+        @main_display = main_display
+        @main_display.board_display
+        @main_display.instructions
+        knight_setup(main_display)
         
     end
 
 
 
-    def knight_directions(main_display)
+    def knight_setup(main_display)
         new_knight = Knight.new()
-        new_knight.set_start_end
-        @main_board.set_start(new_knight.start_position,new_knight)
-        round(main_display,new_knight)
+        @new_knight = new_knight
+        @new_knight.set_start_end
+        @main_board.set_start(@new_knight.start_position,@new_knight)
         
     end
 
-    def round(main_display,knight)
-    main_display.board_display
-    main_display.instructions
+    def piece_selection(piece = @new_knight)
+        return piece
+    end
+    def round
+    piece = piece_selection()
+    @main_display.board_display
+    @main_display.instructions
+        binding.pry
     end
 end
 
 new_game = Main.new()
 new_game.home_screen
+new_game.piece_selection
+new_game.round
