2636bd98369d561b5b7d3493324b69bff55eb9b2
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-11-05 22:36:38 -0700

    created vertice class to create knew grid vertices

---
+++ Chess_Objects/knight_data/vertice.rb
@@ -0,0 +1,7 @@
+class Vertice
+attr_accessor :knight, :board
+    def initialize(knight,board)
+        @knight = knight
+        @board = board
+    end
+end
--- Chess_Objects/knight_data/weighted_edge_graph.rb
+++ Chess_Objects/knight_data/weighted_edge_graph.rb
@@ -1,8 +1,18 @@
+require_relative "vertice"
 class WeightedEdgeGraph
 
     attr_accessor :piece, :board
     def initialize(piece,board)
         @piece = piece
         @board = board
+    end
+
+    def grid_creation(knight, board)
+
+    end
+
+    def vertice_generator(knight, board)
+        #takes in a new knight and board
+        new_vertice = Vertice.new(knight,board)
     end
 end
