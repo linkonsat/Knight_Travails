81eb2da0b705a58e5c503f9f6379cfe4e0d38e2d
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2021-11-07 23:44:32 -0800

    created find path node that returns a path value

--- Chess_Objects/knight_data/weighted_edge_graph.rb
+++ Chess_Objects/knight_data/weighted_edge_graph.rb
@@ -9,14 +9,27 @@ class WeightedEdgeGraph
         @board = board
     end
 
-    def shortest_path?(piece)
-        if(piece.class == Knight)
-            if (piece.current_position == @piece.end_position)
-                return true
-            else 
-                return false
-            end
+    def shortest_path(grid)
+    end_target = grid[0].knight.end_position
+    end_knight = []
+    grid.each do | item|
+        if (item.knight.current_position == end_target)
+        end_knight.push(item)
         end
+    end
+    return find_path(end_knight)
+    end
+
+    def find_path(target_knight)
+        parent_node_state = [target_knight[0]]
+        path = []
+
+        until parent_node_state[0].nil?
+            path.push(parent_node_state[0].knight.current_position)
+            parent_node_state[0] = parent_node_state[0].parent_edge
+        end
+        binding.pry
+        return path
     end
     def grid_creation
         created_grid = vertice_generator(@piece,@board)
@@ -26,35 +39,38 @@ class WeightedEdgeGraph
     def vertice_generator(knight, board)
         #takes in a new knight and board
         grid = []
-        grid.push([Vertice.new(knight,board)])
-        grid.push(new_list_level(grid[0]))
+  
+        grid.push(Vertice.new(knight,board))
+        grid.concat(new_list_level(grid[0]))
+
+        shortest_path(grid)
         binding.pry
 
         
     end
-
+    
     def new_list_level(vertice)
         
         new_list = []
-        new_edges = vertice[0].knight.legal_moves_knight
+        new_edges = vertice.knight.legal_moves_knight
         i = 0
         new_vertices = 0
         until i > new_edges.length - 1 
-            if (vertice[0].knight.in_board?(new_edges[i],vertice[0].board))
+            if (vertice.knight.in_board?(new_edges[i],vertice.board))
                 new_board = GameBoard.new()
-                new_knight = Knight.new(update_knight(new_edges[i],vertice[0].knight))
+                new_knight = Knight.new(update_knight(new_edges[i],vertice.knight))
             new_list.push(Vertice.new(new_knight,new_board.set_new_position(new_knight.current_position,new_knight)))   
             new_list[new_vertices].set_parent_edge(vertice)
             new_vertices += 1
             end
             
-            if(new_list[new_vertices - 1] != nil && new_list[new_vertices - 1].knight.current_position == vertice[0].knight.end_position)
+            if(new_list[new_vertices - 1] != nil && new_list[new_vertices - 1].knight.current_position == vertice.knight.end_position)
                 i += new_edges.length
             else
             i += 1
             end
         end
-        vertice[0].set_edges(new_list)
+        vertice.set_edges(new_list)
         return new_list
     end
 
